14b916022d57d950a1bd97cc6dfba0f9f884aa20
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2022-02-21 18:22:41 -0800

    LeagueInvadersSetup

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	LeagueInvaders(){
+		frame = new JFrame();
+	}
+	void setup(){
+		frame.setSize(WIDTH, HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+public static void main(String[] args) {
+	LeagueInvaders ln = new LeagueInvaders();
+	ln.setup();
+}
 }
